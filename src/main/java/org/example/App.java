0e358b3516d4b7acbb9304f673e0e4c7d4928db0
johnt4758 <Johnathanmanuel@gmail.com>
2022-03-12 20:30:35 -0700

    Added in comments for better understanding of code and how it functions

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,17 +7,23 @@ public class App
     public static void main( String[] args )
 
     {
+        //Basic setup for user input
         Scanner userScanner = new Scanner(System.in);
 
+        //Opening lines for the dragon's cave project
+        //these lines can also be added to its own method for cleaner looking code
         System.out.println( "You find yourself suddenly in world full of dragons" );
         System.out.println("You see two caves. In one cave, the dragon is friendly");
         System.out.println("he will share his treasure with you. The other dragon");
         System.out.println("is greedy and hungry and will eat you on sight.");
         System.out.println("Which cave will you choose? (1 or 2)");
 
+        //using the scanner object to get the users input in the terminal
         String userInput = userScanner.nextLine();
         System.out.println(userInput + " huh?");
 
+        //using basic if statements to determine what the user wrote in the console
+        //appropriate responses follow
         if (userInput.equals("1"))
         {
             System.out.println("You approach the cave slowly...");
